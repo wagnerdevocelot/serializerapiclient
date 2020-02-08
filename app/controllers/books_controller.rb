619f94a1242799f216abd49427d0a0245a9c3047
Joao Almeida <wagner.dev1337@gmail.com>
2020-02-07 21:50:46 -0300

    adicionando o caminho base da api, passando os metodos e o endpoint na index e parseando para ruby

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,15 @@ class BooksController < ApplicationController
 
     require 'rest_client'
 
+    API_BASE_URL = "https://serializerapi.herokuapp.com"
+
+    def index
+        uri = "#{API_BASE_URL}/books.json"
+        rest_resource = RestClient::Resource.new(uri)
+        books = rest_resource.get
+        @books = JSON.parse(books, :symbolize_names => true)
+    end
 
 
-end 
+
+end
